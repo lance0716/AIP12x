1edc139930a3432ef8cf877b55c67e961c85b884
lance0716 <58470167+lance0716@users.noreply.github.com>
2019-12-13 16:32:26 +0800

    Add files via upload

--- AIp12x/tictactoegameLance.py
+++ AIp12x/tictactoegameLance.py
@@ -119,37 +119,7 @@ def isBoardFull(board):
             return False
     return True
 
-###=== (5.2) 程式宣告 ===###  
-app = Flask(__name__)  # __name__ 代表目前執行的模組
-
-
-###=== (5.3) LINE介面密碼 ===### (參考3.3)
-##== (1) Channel Access Token
-line_bot_api = LineBotApi("zX5wosMZDhR00JwpEwCtKCrOGZc/y90rFLsJFv9qhwNEvR29DtFmWUlLzVSDHIeYc09TJ3AIh7XwmV9eduCViUAIgWAC9NMEFWvxU/nlFUg1jPjKLbEAH5p/gNHTBKJIjuZKbf5m5HgVw4rFkSu9vwdB04t89/1O/w1cDnyilFU=")  #-- YOUR_CHANNEL_ACCESS_TOKEN
-##== (2) Channel Secret
-handler = WebhookHandler("d744d023067e2c4d4717a61d8081ac18")  #-- YOUR_CHANNEL_SECRET
-
-
-###=== (5.5) 處理訊息  ===###
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    print(event)
-    if event.message.id == "100001":
-        return
-    text = event.message.text
-    if (text=="Hi"):
-        reply_text = "Hello"
-        #Your user ID
-    elif(text=="你好"): 
-        reply_text = "你好啊..."
-    elif(text=="機器人"):
-        reply_text = "有！我是GAME機器人！"
-    else:  # 如果非以上的選項，就會學你說話
-        reply_text = text
-    message = TextSendMessage(reply_text)
-    line_bot_api.reply_message(event.reply_token, message)
-
-###=== (5.6) 執行程式  ===###
+def game():
 import random
 print('Welcome to Tic Tac Toe!')
 
@@ -202,12 +172,47 @@ while True:
         break
 
 
-'''
-Distinguish the difference between shallow copy and deep copy in python:
-    shallow copy:不會產生一個獨立的物件單獨存在，他只是將原有的資料塊打上一個新標籤，所以當其中一個標籤被改變的時候，資料塊就會發生變化，另一個標籤也會隨之改變。
-    deep copy:將被複制物件完全再複製一遍作為獨立的新個體單獨存在。所以改變原有被複制物件不會對已經複製出來的新物件產生影響。
 
-'''
+
+
+
+
+
+###=== (5.2) 程式宣告 ===###  
+app = Flask(__name__)  # __name__ 代表目前執行的模組
+
+
+###=== (5.3) LINE介面密碼 ===### (參考3.3)
+##== (1) Channel Access Token
+line_bot_api = LineBotApi("zX5wosMZDhR00JwpEwCtKCrOGZc/y90rFLsJFv9qhwNEvR29DtFmWUlLzVSDHIeYc09TJ3AIh7XwmV9eduCViUAIgWAC9NMEFWvxU/nlFUg1jPjKLbEAH5p/gNHTBKJIjuZKbf5m5HgVw4rFkSu9vwdB04t89/1O/w1cDnyilFU=")  #-- YOUR_CHANNEL_ACCESS_TOKEN
+##== (2) Channel Secret
+handler = WebhookHandler("d744d023067e2c4d4717a61d8081ac18")  #-- YOUR_CHANNEL_SECRET
+
+
+###=== (5.5) 處理訊息  ===###
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    print(event)
+    if event.message.id == "100001":
+        return
+    text = event.message.text
+    if (text=="Hi"):
+        reply_text = "Hello"
+        #Your user ID
+    elif(text=="你好"): 
+        reply_text = "你好啊..."
+    elif(text=="機器人"):
+        reply_text = "有！我是GAME機器人！"
+    elif(text=="game"):
+        game()
+    else:  # 如果非以上的選項，就會學你說話
+        reply_text = text
+    message = TextSendMessage(reply_text)
+    line_bot_api.reply_message(event.reply_token, message)
+
+###=== (5.6) 執行程式  ===###
+
+
 
 ###=== (5.6) 執行程式  ===###
 import os
