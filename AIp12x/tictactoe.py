0231390c2bd2ddacf1540d8684c3903489385e20
lance0716 <58470167+lance0716@users.noreply.github.com>
2019-12-14 00:17:53 +0800

    Add files via upload

--- AIp12x/tictactoe.py
+++ AIp12x/tictactoe.py
@@ -124,8 +124,10 @@ def isBoardFull(board):
 
 def game():
 
-    print('Welcome to Tic Tac Toe!')
-
+    #print('Welcome to Tic Tac Toe!')
+    reply_text = "Welcome to Tic Tac Toe!"
+     message = TextSendMessage(reply_text)
+    line_bot_api.reply_message(event.reply_token, message)
     while True:
 
         theBoard = [' '] * 10
@@ -220,9 +222,12 @@ def handle_message(event):
     elif(text=="機器人"):
         reply_text = "有！我是game機器人"
     elif(text=="game"):
+        game()
         #reply_text = "game"
-        #print("Welcome to Tic Tac Toe!",reply_text)
-        line_bot_api.reply_message(event.reply_token, TextSendMessage("Welcome to Tic Tac Toe!"))
+        #reply_text = "Welcome to Tic Tac Toe!"
+        #message = TextSendMessage(reply_text)
+        #line_bot_api.reply_message(event.reply_token, message)
+        #   
         #message = TextSendMessage(reply_text)
         #line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
 
