09baf63b277533fff4c91e0405a183ea9673f8b6
lance0716 <58470167+lance0716@users.noreply.github.com>
2019-12-14 01:11:50 +0800

    Add files via upload

--- AIp12x/tictactoe.py
+++ AIp12x/tictactoe.py
@@ -249,7 +249,7 @@ def handle_message(event):
         message = TextSendMessage(reply_text)
         line_bot_api.reply_message(event.reply_token, message)
 
-        while True:
+        while 1:
 
             theBoard = [' '] * 10
             playerLetter, computerLetter = inputPlayerLetter()
