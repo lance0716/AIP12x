ee1089974ac98757f2ac99a1fd42d6a783dd12df
lance0716 <58470167+lance0716@users.noreply.github.com>
2019-12-13 21:28:12 +0800

    Add files via upload

--- AIp12x/tictactoe.py
+++ AIp12x/tictactoe.py
@@ -9,120 +9,6 @@ from flask import url_for, redirect, render_template, Markup
 import numpy as np
 import pandas as pd
 import random
-def drawBoard(board):
-
-    print(board[7] + '|' + board[8] + '|' + board[9])
-    print('-+-+-')
-    print(board[4] + '|' + board[5] + '|' + board[6])
-    print('-+-+-')
-    print(board[1] + '|' + board[2] + '|' + board[3])
-
-def inputPlayerLetter():
-
-    letter = ''
-    while not (letter == 'O' or letter == 'X'):
-        print('Do you want to be O or X?')
-        letter = input().upper()
-
-    if letter == 'X':
-        return ['X', 'O']
-    else:
-        return ['O', 'X']
-
-def whoGoesFirst():
-
-    print('Do you want to go first?')
-    gofirstornot = input().upper()
-    if gofirstornot != 'Y':
-        return 'computer'
-    else:
-        return 'player'
-
-def makeMove(board, letter, move):
-    board[move] = letter
-
-def isWinner(bo, le):
-
-    return ((bo[7] == le and bo[8] == le and bo[9] == le) or #  top
-    (bo[4] == le and bo[5] == le and bo[6] == le) or #  middle
-    (bo[1] == le and bo[2] == le and bo[3] == le) or #  bottom
-    (bo[7] == le and bo[4] == le and bo[1] == le) or # down the left side
-    (bo[8] == le and bo[5] == le and bo[2] == le) or # down the middle
-    (bo[9] == le and bo[6] == le and bo[3] == le) or # down the right side
-    (bo[7] == le and bo[5] == le and bo[3] == le) or # diagonal
-    (bo[9] == le and bo[5] == le and bo[1] == le)) # diagonal
-
-def getBoardCopy(board):
-
-    boardCopy = []
-    for i in board:
-        boardCopy.append(i)
-    return boardCopy
-
-def isSpaceFree(board, move):
-
-    return board[move] == ' '
-
-def getPlayerMove(board):
-
-    move = ' '
-    while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
-        print('What is your next move? (1-9)')
-        move = input()
-    return int(move)
-
-def chooseRandomMoveFromList(board, movesList):
-
-    possibleMoves = []
-    for i in movesList:
-        if isSpaceFree(board, i):
-            possibleMoves.append(i)
-
-    if len(possibleMoves) != 0:
-        return random.choice(possibleMoves)
-    else:
-        return None
-
-def getComputerMove(board, computerLetter):
-
-    if computerLetter == 'X':
-        playerLetter = 'O'
-    else:
-        playerLetter = 'X'
-
-    # AI:
-    for i in range(1, 10):
-        boardCopy = getBoardCopy(board)
-        if isSpaceFree(boardCopy, i):
-            makeMove(boardCopy, computerLetter, i)
-            if isWinner(boardCopy, computerLetter):
-                return i
-
-
-    for i in range(1, 10):
-        boardCopy = getBoardCopy(board)
-        if isSpaceFree(boardCopy, i):
-            makeMove(boardCopy, playerLetter, i)
-            if isWinner(boardCopy, playerLetter):
-                return i
-
-    move = chooseRandomMoveFromList(board, [1, 3, 7, 9])
-    if move != None:
-        return move
-
-    if isSpaceFree(board, 5):
-        return 5
-
-    return chooseRandomMoveFromList(board, [2, 4, 6, 8])
-
-def isBoardFull(board):
-
-    for i in range(1, 10):
-        if isSpaceFree(board, i):
-            return False
-    return True
-
-
 
 
 ###=== (5.2) 設定對話(kk,openF,answerF) ===###
@@ -171,7 +57,7 @@ def handle_message(event):
         reply_text = "ooxx game"
     elif(text=="介紹"):    reply_text = openF1
     elif(text=="game"):
-        print('Welcome to Tic Tac Toe!')
+        print("game")
 
 
 ###=== (5.6) 執行程式  ===###
