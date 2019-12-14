48d0aa744e12d530dc9db4e6c6828a581d59d39a
lance0716 <58470167+lance0716@users.noreply.github.com>
2019-12-14 17:19:39 +0800

    Add files via upload

--- AIp12x/tictactoe.py
+++ AIp12x/tictactoe.py
@@ -250,23 +250,20 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
 
         while True:
-            @handler.add(MessageEvent, message=TextMessage)
-            def handle_text_message(event):
-                text = event.message.text
-                reply_text = "".join(["while",text])
-                #line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
+            reply_text = "".join(["while",text])
+            #line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
 
-                theBoard = [' '] * 10
-                playerLetter, computerLetter = inputPlayerLetter()
-                turn = whoGoesFirst()
-                #print('The ' + turn + ' will go first.')
-                reply_text = "".join(["設定 X=",text])
-                reply_text = "The " + turn + " will go first.!"
-                message = TextSendMessage(reply_text)
-                line_bot_api.reply_message(event.reply_token, message)
-                gameIsPlaying = True
+            theBoard = [' '] * 10
+            playerLetter, computerLetter = inputPlayerLetter()
+            turn = whoGoesFirst()
+            #print('The ' + turn + ' will go first.')
+            reply_text = "".join(["設定 X=",text])
+            reply_text = "The " + turn + " will go first.!"
+            message = TextSendMessage(reply_text)
+            line_bot_api.reply_message(event.reply_token, message)
+            gameIsPlaying = True
 
-                theBoard = [' '] * 10
+            theBoard = [' '] * 10
     else:  # 如果非以上的選項，就會學你說話
         reply_text = text
     message = TextSendMessage(reply_text)
