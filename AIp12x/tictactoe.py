3553c3251731356b94d9f4bb4e0bdaba21cbcb19
lance0716 <58470167+lance0716@users.noreply.github.com>
2019-12-14 17:15:36 +0800

    Add files via upload

--- AIp12x/tictactoe.py
+++ AIp12x/tictactoe.py
@@ -246,20 +246,21 @@ def handle_message(event):
         reply_text = "有！我是game機器人"
     elif(text=="game"):
         #game()
-        reply_text = "Welcome to Tic Tac Toe!"
+        text = "Welcome to Tic Tac Toe!"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
 
         while True:
             @handler.add(MessageEvent, message=TextMessage)
             def handle_text_message(event):
                 text = event.message.text
-                reply_text = "While"
-                line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
+                reply_text = "".join(["while",text])
+                #line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
 
                 theBoard = [' '] * 10
                 playerLetter, computerLetter = inputPlayerLetter()
                 turn = whoGoesFirst()
                 #print('The ' + turn + ' will go first.')
+                reply_text = "".join(["設定 X=",text])
                 reply_text = "The " + turn + " will go first.!"
                 message = TextSendMessage(reply_text)
                 line_bot_api.reply_message(event.reply_token, message)
