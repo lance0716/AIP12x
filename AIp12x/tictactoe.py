1a483d5e616488c80f68bae805cfd1933c6a4baf
lance0716 <58470167+lance0716@users.noreply.github.com>
2019-12-14 10:24:42 +0800

    Add files via upload

--- AIp12x/tictactoe.py
+++ AIp12x/tictactoe.py
@@ -250,9 +250,6 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, message)
 
         while True:
-            @handler.add(MessageEvent, message=TextMessage)
-            def handle_message(event):
-            text = event.message.text
             reply_text = "while"
             message = TextSendMessage(reply_text)
             line_bot_api.reply_message(event.reply_token, message)
